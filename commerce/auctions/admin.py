52245ddfda1d39d785add7f277e865bd7036e636
Amirhossein <dollerbankas@gmail.com>
2020-11-07 17:14:41 -1200

    all tables in models.py aaded to admin user

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Auction_listing, Categories, Bids, User_listing, Comments
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Auction_listing)
+admin.site.register(Comments)
+admin.site.register(Categories)
+admin.site.register(Bids)
+admin.site.register(User_listing)
